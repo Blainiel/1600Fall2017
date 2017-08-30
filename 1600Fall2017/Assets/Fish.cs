d7331efed003c917e3b9a238d32f71c20b938ec1
Unknown <iamsoprofessionalforme@gmail.com>
2017-08-30 11:41:18 -0600

    Class Work

---
+++ 1600Fall2017/Assets/Fish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Animal {
+	// Use this for initialization
+	    public override void Start(){
+        base.Start();
+        Swim();
+    }
+	 
+	// Update is called once per frame
+	void Update () {
+}
+
+void Swim () {
+    print(this.name + " Swims");
+}
+
+}
---
+++ 1600Fall2017/Assets/Mammal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mammal : Animal {
+
+ public override void Start(){
+        base.Start();
+        Livebirth();
+    }
+	 
+void Livebirth () {
+    print(this.name + " Gives Live Birth");
+}
