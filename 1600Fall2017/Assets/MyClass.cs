e75d2d4d9b563bab4807573aa4b9c18a0b18c407
Unknown <iamsoprofessionalforme@gmail.com>
2017-09-06 11:53:14 -0600

    MyClass added

---
+++ 1600Fall2017/Assets/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "Oh no.... I might actually learn things in this class";
+
+
+	// Use this for initialization
+	void Start () {
+		print(myString);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
