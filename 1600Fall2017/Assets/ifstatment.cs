eda55454befc60e97d189578987075717418cc3e
Unknown <iamsoprofessionalforme@gmail.com>
2017-09-11 11:55:33 -0600

    Ifstatments

---
+++ 1600Fall2017/Assets/ifstatment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifstatment : MonoBehaviour {
+
+public int num1;
+public int num2;
+public int value;
+
+public bool canPlay = true; 
+
+public string input;
+private string password = "G@m3Pl@y";
+	// Use this for initialization
+	void Start () {
+		if (num1 + num2 == value)
+		{
+			print(value);
+		}
+if (input == password){
+print("you know the password");}
+
+if (canPlay){
+print ("Play");}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
