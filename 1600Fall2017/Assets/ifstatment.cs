b0079a83fd3ea22b614b08365a8e04b310215946
Unknown <iamsoprofessionalforme@gmail.com>
2017-09-13 11:02:32 -0600

    Modified If statments

--- 1600Fall2017/Assets/ifstatment.cs
+++ 1600Fall2017/Assets/ifstatment.cs
@@ -1,32 +1,62 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ifstatment : MonoBehaviour {
 
 public int num1;
 public int num2;
 public int value;
-
+public int X;
 public bool canPlay = true; 
-
 public string input;
-private string password = "G@m3Pl@y";
+private string password = "ironmansucks";
+public string input2;
+public string Username = "Batman";
+public bool OhNo = true;
+public bool grapes;
+public bool pain;
 	// Use this for initialization
 	void Start () {
+
+		if (pain){
+			print("undoubtedly");
+		}
+
+		if (grapes){
+			print("Yas Queen");
+		}
+
+		if (X + num1 == 26){
+			print("Mathmatics!");
+		}
+
+		if (X == 24){
+			print("Okay!");
+		}
+
+		if (OhNo){
+			print("I'm in a lot of pain....");
+		}
+
+		if(input == password){
+		print("You know the password!");}
+
+		if(input2 == Username){
+		print("Good Username!");}
+
+		if (4 + 6 == 10){
+			print("Yes.....");
+		}
+
 		if (num1 + num2 == value)
 		{
 			print(value);
 		}
-if (input == password){
-print("you know the password");}
 
 if (canPlay){
 print ("Play");}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
