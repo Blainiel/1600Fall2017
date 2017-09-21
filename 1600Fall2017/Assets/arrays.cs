4147feb4e0dfa513b3a0403a76156bc65d25c29e
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-09-20 19:25:53 -0600

    Arrays assignment

---
+++ 1600Fall2017/Assets/arrays.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrays : MonoBehaviour {
+char[] vowels = new char [5];
+vowels[0] = "a";
+vowels[1] = "e";
+vowels[2] = "i";
+vowels[3] = "o";
+vowels[4] = "u";
+console.WriteLine (vowels[4]);
+for (int i = 0; i< vowels.Length; i++)
+Console.Write (vowels [i]);
+
+// i dont know what i'm fucking doing.
+
+}
