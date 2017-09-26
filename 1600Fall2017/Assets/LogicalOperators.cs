8600ce69f44c4f16d261cdb81995ffa0166e99e9
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-09-25 22:53:55 -0600

    Logical Operators Assignment

---
+++ 1600Fall2017/Assets/LogicalOperators.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOperators : MonoBehaviour {
+
+public bool friends = false;
+public bool loneliness = true;
+public bool A = true;
+public bool B = true;
+public bool what = false;
+public bool why = false;
+	void Start () {
+		
+		if (friends && loneliness){
+			print("Haha me? have friends? Nope.");
+		}
+
+		if (friends || loneliness){
+			print("I like people.");
+		}
+
+		if (!(friends && loneliness)){
+			print("I HAVE FRIENDS!");
+		}
+		if (A && B){
+		print("This is true.");
+	}
+		if (A || B){
+		print("This statment is also true.");
+		}
+		if (!(A && B)){
+		print("This is FALSE! Liar.... mhm");
+		}
+		if (!(A || B)){
+		print("This could be true.");
+		}
+		if (what && why){
+			print("This is false...");
+		}
+		if (what || why){
+			print("This is also false");
+		}
+		if (!(what && why)){
+			print("this is true!");
+		}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
