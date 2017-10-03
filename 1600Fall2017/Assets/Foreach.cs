ba34d4c8ebb8fba6b5066b1fe2a92ed9126fdbf0
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-10-02 18:32:25 -0600

    for each assignment

---
+++ 1600Fall2017/Assets/Foreach.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foreach : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] pets = { "horse", "ferret", "Watson" };
+
+        foreach (string value in pets)
+        {
+            print(value);
+        }
+    }
+
