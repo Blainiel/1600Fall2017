ea7a7785a7c8f09fbf8b14fe68b8dc232e66fa8f
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-10-04 19:54:18 -0600

    Scope and Access

---
+++ 1600Fall2017/Assets/ScopeAndAccess.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccess : MonoBehaviour {
+
+private int health = 100;
+
+public int score = 100;
+
+void Box () {
+	int newScore = 10;
+	print(newScore);
+
+	// this box can see everything... but the feild can't see into this.
+
+	void Box2 (){
+		int health = 50;
+		print(health);
+		// this box can also see everything..... but nothing else can see into this.
+		// I can also change values.
+	}
+}
+
+}
