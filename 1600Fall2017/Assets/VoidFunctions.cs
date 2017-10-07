14bfbc770edf1f92908c90de4c3697259bfc30b7
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-10-06 21:43:01 -0600

    Void Functions

---
+++ 1600Fall2017/Assets/VoidFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// void means its an empty function.
+	void Box (){
+
+	}
+	// things inside the box can see out, things outside the box can't see in.
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
