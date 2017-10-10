47db0958f61855301f3d38a588d790d9633ad980
Blainiel <31219241+Blainiel@users.noreply.github.com>
2017-10-10 01:30:37 -0600

    functions with returns

---
+++ 1600Fall2017/Assets/MathFunctions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		myScore = ReturnNum(health, ammo);
+		myPassword = ReturnPassword (myPassword);
+		permission = YesNo (permission);
+	}
+	public bool permission;
+	public int myScore;
+
+	public int health;
+	public int ammo;
+
+	public string myPassword = "Batman";
+	int ReturnNum (int _num, int _num2){
+		return _num + _num2;}
+
+	int ReturnZero (){
+		return 0; 
+	}
+	void SendZero (){
+		myScore = 0;
+	}
+
+	string ReturnPassword (string _login){
+		if(_login == "ou812") {
+			return "Password Correct";
+			} else {
+
+		return "Incorrect";}
+	}
+
+	bool YesNo (bool _Yes){
+		return true;
+	}
+
+
+}
